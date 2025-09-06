553f07f8f7580550e04ac0c5d26b60a6006d4183
mohammed <khamedkh297@gmail.com>
2025-09-06 19:26:24 +0100

    Web Terminal

--- django_commands_suite/utils.py
+++ django_commands_suite/utils.py
@@ -1,4 +1,6 @@
+from django.core.management import call_command
 from .models import CommandLog
+import io
 
 def log_command(name, args=None, status="success", message=""):
    CommandLog.objects.create(
@@ -7,3 +9,32 @@ def log_command(name, args=None, status="success", message=""):
       status=status,
       message=message
    )
+
+
+
+
+
+def run_command(command_name: str, *args, **kwargs):
+    """
+    Run a Django management command from code (instead of CLI).
+    It will capture output and log it automatically into CommandLog.
+    """
+    out = io.StringIO()
+    try:
+        call_command(command_name, *args, stdout=out, stderr=out, **kwargs)
+        output = out.getvalue()
+        CommandLog.objects.create(
+            command=command_name,
+            status="success",
+            message=output,
+            options=str(kwargs)
+        )
+        return output
+    except Exception as e:
+        CommandLog.objects.create(
+            command=command_name,
+            status="error",
+            message=str(e),
+            options=str(kwargs)
+        )
+        raise e

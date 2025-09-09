c71afeb623a172645edf0168889ae62a2271c485
mohammed <khamedkh297@gmail.com>
2025-09-09 18:53:38 +0100

    readthedocs up

--- docs/conf.py
+++ docs/conf.py
@@ -8,17 +8,20 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 project = "django-commands-suite"
+copyright = '2025, Mohammed Taha Khamed'
 author = "Mohammed Taha Khamed"
-version = "0.2"
 release = "0.2.5"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",      
+    "sphinx.ext.viewcode",     
+    "sphinx.ext.napoleon",     
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
+# -- Options for HTML output -------------------------------------------------
+html_theme = "furo"
+html_static_path = ["_static"]
